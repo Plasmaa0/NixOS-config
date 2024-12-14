ec20c7bb84e064ae13dfbb711346d143c1ed5d3e
Andrey Kuprin <plasmaa0@gmail.com>
2024-12-15 00:47:35 +0300

    removed -q from alejandra

--- scripts/update-upgrade.sh
+++ scripts/update-upgrade.sh
@@ -1,5 +1,5 @@
 sudo rm /etc/nixos/* -rf
-alejandra -q $(fd .nix)
+alejandra $(fd .nix)
 sudo cp configuration/* /etc/nixos -r
 sudo nix-channel --update
 sudo nixos-rebuild boot --flake /etc/nixos --upgrade --show-trace
