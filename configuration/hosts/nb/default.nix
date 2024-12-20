33c766dbc14e77ef0f2bf1db06207224ed935fe8
Andrey Kuprin <plasmaa0@gmail.com>
2024-12-20 14:49:13 +0300

    bluetooth

--- configuration/hosts/nb/default.nix
+++ configuration/hosts/nb/default.nix
@@ -5,5 +5,7 @@
     ../common/modules/steam.nix
     ../common/modules/vial.nix
     ../common/modules/HighDPI.nix
+    ../common/modules/bluetooth.nix
   ];
+  hardware.bluetooth.powerOnBoot = false;
 }
