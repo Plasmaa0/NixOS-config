d4897e496c8eb416877c0eb4c3ef8decae592c26
Andrey Kuprin <plasmaa0@gmail.com>
2024-12-28 15:48:08 +0300

    plymouth

--- configuration/hosts/common/bootloader.nix
+++ configuration/hosts/common/bootloader.nix
@@ -24,28 +24,30 @@
       };
     };
     # plymouth settings
-    # plymouth = {
-    #   enable = true;
-    #   theme = "rings";
-    #   themePackages = with pkgs; [
-    #     (adi1090x-plymouth-themes.override {selected_themes = [ "rings" ];})
-    #   ];
-    # };
+    plymouth = let
+      theme_name = "colorful_loop";
+    in {
+      enable = true;
+      theme = theme_name;
+      themePackages = with pkgs; [
+        (adi1090x-plymouth-themes.override {selected_themes = [theme_name];})
+      ];
+    };
     # # Enable "Silent Boot"
-    # consoleLogLevel = 0;
-    # initrd.verbose = false;
-    # kernelParams = [
-    #   "quiet"
-    #   "splash"
-    #   "boot.shell_on_fail"
-    #   "loglevel=3"
-    #   "rd.systemd.show_status=false"
-    #   "rd.udev.log_level=3"
-    #   "udev.log_priority=3"
-    # ];
-    # # Hide the OS choice for bootloaders.
-    # # It's still possible to open the bootloader list by pressing any key
-    # # It will just not appear on screen unless a key is pressed
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+    # Hide the OS choice for bootloaders.
+    # It's still possible to open the bootloader list by pressing any key
+    # It will just not appear on screen unless a key is pressed
     # loader.timeout = 0;
   };
 }
