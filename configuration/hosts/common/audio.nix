40d81e81c7311098a4499cc6361f4b85e552f996
Andrey Kuprin <plasmaa0@gmail.com>
2025-01-07 01:58:34 +0300

    audio upd

--- configuration/hosts/common/audio.nix
+++ configuration/hosts/common/audio.nix
@@ -3,6 +3,9 @@
 
   hardware.pulseaudio.enable = false;
 
+  # avahi required for service discovery
+  services.avahi.enable = true;
+
   services.pipewire = {
     enable = true;
 
@@ -13,5 +16,37 @@
 
     pulse.enable = true;
     jack.enable = true;
+
+    wireplumber.extraConfig."10-bluez" = {
+      "monitor.bluez.properties" = {
+        "bluez5.enable-sbc-xq" = true;
+        "bluez5.enable-msbc" = true;
+        "bluez5.enable-hw-volume" = true;
+        "bluez5.roles" = [
+          "hsp_hs"
+          "hsp_ag"
+          "hfp_hf"
+          "hfp_ag"
+        ];
+      };
+    };
+
+    # opens UDP ports 6001-6002
+    raopOpenFirewall = true;
+
+    extraConfig.pipewire = {
+      "10-airplay" = {
+        "context.modules" = [
+          {
+            name = "libpipewire-module-raop-discover";
+
+            # increase the buffer size if you get dropouts/glitches
+            # args = {
+            #   "raop.latency.ms" = 500;
+            # };
+          }
+        ];
+      };
+    };
   };
 }
