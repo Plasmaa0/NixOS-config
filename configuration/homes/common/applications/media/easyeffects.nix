8d9eb6d0029284557a2deed752fb13a11b08e0ed
Andrey Kuprin <plasmaa0@gmail.com>
2025-01-09 23:12:19 +0300

    disabled startup of easyeffects by default

--- configuration/homes/common/applications/media/easyeffects.nix
+++ configuration/homes/common/applications/media/easyeffects.nix
@@ -1,5 +1,10 @@
-{...}: {
+{pkgs, ...}: {
   services.easyeffects = {
     enable = true;
   };
+
+  # don't start by default
+  # override default value which is
+  # Install.WantedBy = [ "graphical-session.target" ];
+  systemd.user.services.easyeffects.Install.WantedBy = pkgs.lib.mkForce [];
 }
