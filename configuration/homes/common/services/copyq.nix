329d0ae122a4f4249c956a39b7f4aa5863f948be
Andrey Kuprin <plasmaa0@gmail.com>
2025-01-13 22:07:59 +0300

    copyq clip-file program WIP
    Former-commit-id: 15771ef45fc716bc1108379c2684590e771df63e

--- configuration/homes/common/services/copyq.nix
+++ configuration/homes/common/services/copyq.nix
@@ -1,4 +1,28 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   services.copyq.enable = true;
   home.persistence."/persist/home/${config.home.username}".directories = [".local/share/copyq"];
+  # FIXME this script now works only with images
+  home.packages = [
+    (pkgs.writeShellApplication {
+      name = "clip-file";
+      runtimeInputs = with pkgs; [copyq xdg-utils];
+      text = ''
+        set +u
+        file=$1
+        if [ -z "$file" ]
+        then
+          echo "Please, pass filename"
+          exit 1
+        else
+          mimetype=$(xdg-mime query filetype "$file")
+          echo Detected mime-type "$mimetype"
+          copyq write "$mimetype" - < "$file" && copyq select 0 && echo "Copied!" || echo "Failure!"
+        fi
+      '';
+    })
+  ];
 }
