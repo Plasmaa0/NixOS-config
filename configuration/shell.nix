13777275a455900bbf605595091458cecc24261d
Andrey Kuprin <plasmaa0@gmail.com>
2025-01-17 12:57:18 +0300

    flake.nix refactor devshell upd

--- configuration/shell.nix
+++ configuration/shell.nix
@@ -1,11 +1,6 @@
 {pkgs ? (import ./nixpkgs.nix) {}}: {
   default = pkgs.mkShell {
     name = "nix-config";
-    # shellHook = ''
-    #   ${pkgs.fish}/bin/fish
-    #   exit
-    # '';
-    # Enable experimental features without having to specify the argument
     NIX_CONFIG = "experimental-features = nix-command flakes";
     nativeBuildInputs = with pkgs; [
       nix
