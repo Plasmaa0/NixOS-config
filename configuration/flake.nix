6bb73617aec656dce6480b8d901828ee116bddf4
Andrey Kuprin <plasmaa0@gmail.com>
2025-01-17 12:57:18 +0300

    flake.nix refactor devshell upd
    Former-commit-id: 13777275a455900bbf605595091458cecc24261d

--- configuration/flake.nix
+++ configuration/flake.nix
@@ -30,50 +30,13 @@
     pkgs = import nixpkgs {
       config.allowUnfree = true;
       config.nvidia.acceptLicense = true;
-      system = "x86_64-linux";
+      inherit system;
     };
-    mkSystem = {
-      host,
-      homes,
-    }:
-      nixpkgs.lib.nixosSystem {
-        modules = [
-          ./hosts/${host}
-          {
-            networking.hostName = host;
-            users.users =
-              (nixpkgs.lib.genAttrs homes (user: {
-                isNormalUser = true;
-                description = user;
-                extraGroups = ["networkmanager" "wheel"];
-                password = import ./homes/common/secrets/${user}_password.nix;
-              }))
-              // {root.hashedPasswordFile = toString ./homes/common/secrets/root_hashed_password;};
-            # https://wiki.nixos.org/wiki/Home_Manager#Workaround_with_home_on_tmpfs_and_standalone_installation
-            system.activationScripts.profile-init.text = nixpkgs.lib.concatStringsSep "\n" (map
-              (user: "ln -sfn /home/${user}/.local/state/nix/profiles/profile /home/${user}/.nix-profile")
-              homes);
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              extraSpecialArgs = {
-                inherit inputs outputs;
-              };
-              users = nixpkgs.lib.genAttrs homes (user: {
-                imports = [
-                  {
-                    home.username = user;
-                    home.homeDirectory = "/home/${user}";
-                  }
-                  ./homes/${user}
-                  inputs.nix-index-database.hmModules.nix-index
-                ];
-              });
-            };
-          }
-        ];
-        specialArgs = {inherit inputs outputs pkgs home-manager;};
-      };
+    mkSystem = import ./mkSystem.nix {inherit nixpkgs inputs outputs pkgs home-manager;};
+    forAllSystems = nixpkgs.lib.genAttrs [
+      "aarch64-linux"
+      "x86_64-linux"
+    ];
   in {
     nixosConfigurations = {
       nb = mkSystem {
@@ -81,5 +44,6 @@
         homes = ["plasmaa0"];
       };
     };
+    devShells = forAllSystems (system: import ./shell.nix {inherit pkgs;});
   };
 }
---
+++ configuration/mkSystem.nix
@@ -0,0 +1,48 @@
+{
+  nixpkgs,
+  inputs,
+  outputs,
+  pkgs,
+  home-manager,
+}: {
+  host,
+  homes,
+}:
+nixpkgs.lib.nixosSystem {
+  modules = [
+    ./hosts/${host}
+    {
+      networking.hostName = host;
+      users.users =
+        (nixpkgs.lib.genAttrs homes (user: {
+          isNormalUser = true;
+          description = user;
+          extraGroups = ["networkmanager" "wheel"];
+          password = import ./homes/common/secrets/${user}_password.nix;
+        }))
+        // {root.hashedPasswordFile = toString ./homes/common/secrets/root_hashed_password;};
+      # https://wiki.nixos.org/wiki/Home_Manager#Workaround_with_home_on_tmpfs_and_standalone_installation
+      system.activationScripts.profile-init.text = nixpkgs.lib.concatStringsSep "\n" (map
+        (user: "ln -sfn /home/${user}/.local/state/nix/profiles/profile /home/${user}/.nix-profile")
+        homes);
+      home-manager = {
+        useGlobalPkgs = true;
+        useUserPackages = true;
+        extraSpecialArgs = {
+          inherit inputs outputs;
+        };
+        users = nixpkgs.lib.genAttrs homes (user: {
+          imports = [
+            {
+              home.username = user;
+              home.homeDirectory = "/home/${user}";
+            }
+            ./homes/${user}
+            inputs.nix-index-database.hmModules.nix-index
+          ];
+        });
+      };
+    }
+  ];
+  specialArgs = {inherit inputs outputs pkgs home-manager;};
+}
