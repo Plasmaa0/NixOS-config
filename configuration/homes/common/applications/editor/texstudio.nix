2b1c3b873548c19d5b56a43c5967dd9aca05f693
Andrey Kuprin <plasmaa0@gmail.com>
2025-01-17 12:55:49 +0300

    texstudio impermanence

--- configuration/homes/common/applications/editor/texstudio.nix
+++ configuration/homes/common/applications/editor/texstudio.nix
@@ -1,5 +1,12 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   home.packages = with pkgs; [texstudio];
+  home.persistence."/persist/home/${config.home.username}".directories = [
+    ".config/texstudio"
+  ];
   mime.list = [
     {
       mimeTypes = ["text/x-tex"];
