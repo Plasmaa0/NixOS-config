7cfdfdfc6ec1830a0d3f38d60828159efa154338
Andrey Kuprin <plasmaa0@gmail.com>
2025-01-17 12:55:29 +0300

    update fish completions on rebuild

--- configuration/homes/common/cli/shell/fish/default.nix
+++ configuration/homes/common/cli/shell/fish/default.nix
@@ -1,10 +1,14 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: {
   home.file."${config.xdg.configHome}/fish/config.fish".source = ./config.fish;
   home.persistence."/persist/home/${config.home.username}".directories = [".local/share/fish" ".jump"];
+  home.activation.fish_update_completions = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    run ${pkgs.fish}/bin/fish -c fish_update_completions || true
+  '';
   home.packages = with pkgs; [
     fish
     fishPlugins.done
