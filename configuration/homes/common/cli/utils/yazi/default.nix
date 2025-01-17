1574ca11e12ed93c8efd3c87139aa5e02343a9df
Andrey Kuprin <plasmaa0@gmail.com>
2025-01-18 00:28:15 +0300

    yazi upd

--- configuration/homes/common/cli/utils/yazi/default.nix
+++ configuration/homes/common/cli/utils/yazi/default.nix
@@ -1,6 +1,20 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+  home.packages = [pkgs.trash-cli];
+  home.persistence."/persist/home/${config.home.username}".directories = [".local/share/Trash"];
   programs.yazi = {
     enable = true;
+    plugins = let
+      plugin-list = builtins.attrNames (builtins.readDir ./plugins);
+      remove-yazi-extension = filename: lib.elemAt (builtins.split ".yazi" filename) 0;
+      sanitized-plugin-list = map remove-yazi-extension plugin-list;
+      plugins-result = lib.genAttrs sanitized-plugin-list (name: ./plugins/${name}.yazi);
+    in
+      plugins-result;
     keymap = {
       manager.prepend_keymap = let
         echo = "${pkgs.coreutils}/bin/echo";
@@ -12,15 +26,20 @@
         dragon = "${pkgs.xdragon}/bin/dragon";
       in [
         {
-          # drag&drop FROM yazi popup for current selection
+          on = "u";
+          run = "plugin restore";
+          desc = "Restore last deleted files/folders";
+        }
+        {
+          desc = "drag&drop FROM yazi. Popup from current selection.";
           on = ["<C-o>"];
           run = ''shell '${dragon} -A -x -T "$@"' '';
         }
         {
-          # drag&drop TO yazi current directory
+          desc = "drag&drop TO yazi current directory";
           on = ["<C-i>"];
           run = let
-            files = ''"$(${dragon} -t -p -k -T)"'';
+            files = ''"$(${dragon} -t -p -k -T -x)"'';
             destination = ''"$(${realpath} "$0" | ${xargs} ${dirname})"''; #yazi cursor is always hovering directory or file so dirname(realpath($0)) will be current directory
             getfiles = ''${cp} ${files} ${destination}'';
           in [
@@ -28,7 +47,7 @@
           ];
         }
         {
-          # yank current selection in system clipboard
+          desc = "yank current selection in system clipboard";
           on = ["y"];
           run = let
             to_uri = ''{ for i in "$@"; do ${echo} -en "file://$(${realpath} "''${i}")\n"; done }'';
