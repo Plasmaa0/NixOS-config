47153195482d3ce6b4b88078cb481d238aa0309b
Andrey Kuprin <plasmaa0@gmail.com>
2025-01-29 17:35:59 +0300

    enabled nix-direnv

--- configuration/homes/common/cli/utils/direnv.nix
+++ configuration/homes/common/cli/utils/direnv.nix
@@ -1,6 +1,7 @@
 {config, ...}: {
   programs.direnv = {
     enable = true;
+    nix-direnv.enable = true;
   };
   home.persistence."/persist/home/${config.home.username}" = {
     directories = [".local/share/direnv"];
