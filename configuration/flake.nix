ddddffcbff3e2549c30fb5b08112d2debfcfa8f9
Andrey Kuprin <plasmaa0@gmail.com>
2025-02-12 00:14:10 +0300

    added cachix
    Former-commit-id: ed3985e3f0eab7539967ac067f195a5df5e9bcac

--- configuration/flake.nix
+++ configuration/flake.nix
@@ -1,6 +1,15 @@
 {
   description = "Home Manager configuration of plasmaa0";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     # nixos-hardware.url = "github:NixOS/nixos-hardware";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
