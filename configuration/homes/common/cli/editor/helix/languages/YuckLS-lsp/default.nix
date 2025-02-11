8569469faaf8191a286477f88e2f3b95ca9f7ecc
Andrey Kuprin <plasmaa0@gmail.com>
2025-02-12 00:09:53 +0300

    fixed YuckLS lsp version
    Former-commit-id: f06045fb941a4c183170f67bbc09cbd5fb92a0b1

--- configuration/homes/common/cli/editor/helix/languages/YuckLS-lsp/default.nix
+++ configuration/homes/common/cli/editor/helix/languages/YuckLS-lsp/default.nix
@@ -7,7 +7,7 @@ buildDotnetModule {
   name = "yuckls";
   pname = "yuckls";
   src = fetchzip {
-    url = "https://github.com/Eugenenoble2005/YuckLS/archive/refs/heads/main.zip";
+    url = "https://github.com/Eugenenoble2005/YuckLS/archive/ab4c0315cd6c77ef0ed3c620bde0ece48e4a5949.zip";
     hash = "sha256-HhxFVX9BHNydguGFZMd5FNZB06KxF34A9CqTzwJijes=";
   };
 
