3ac4a6e3105b24f236315d5b5e6f414ce02d1e04
Andrey Kuprin <plasmaa0@gmail.com>
2025-02-12 00:13:20 +0300

    removed nvidia-prime-sync specialisation in favor of decreasing build time
    Former-commit-id: 2408babd62681020025cb216a42b4a357e9992b8

--- configuration/hosts/nb/hardware/I_HATE_NVIDIA.nix
+++ configuration/hosts/nb/hardware/I_HATE_NVIDIA.nix
@@ -1,39 +1,14 @@
-{
-  config,
-  lib,
-  ...
-}: let
+{lib, ...}: let
   nixos-hardware = builtins.fetchTarball {
     url = "https://github.com/NixOS/nixos-hardware/archive/f6581f1c3b137086e42a08a906bdada63045f991.tar.gz";
     sha256 = "sha256:1qjivy929fpjf736f78v6hdhv64jgx2m1aff85w1d3cw7c4ppmag";
   };
 in {
-  # NEW CONFIGURATION
   imports = [
-    # -------------------replace here------
-    # ------------------- \/  \/  \/ ------
     "${nixos-hardware}/huawei/machc-wa" # enables prime offload by default. Provides `nvidia-offload` command.
   ];
 
   # disable ppd which is enabled by default in <machc-wa>
   # i need this because i have power management made using tlp
   services.power-profiles-daemon.enable = lib.mkForce false;
-
-  # NixOS supports "specialisations", which allow you to automatically generate
-  # different boot profiles when rebuilding your system.
-  # Because prime offload is enabled by defalut this is reasonable thing to do:
-  # Specialisation which enables prime sync:
-  specialisation = {
-    nvidia-prime-sync.configuration = {
-      system.nixos.tags = ["nvidia-prime-sync"];
-      # enable prime sync INSTEAD of offload
-      hardware.nvidia.prime = {
-        sync.enable = lib.mkOverride 990 true;
-        offload = {
-          enable = lib.mkForce false;
-          enableOffloadCmd = lib.mkIf config.hardware.nvidia.prime.offload.enable false;
-        };
-      };
-    };
-  };
 }
