806d8596ccccf7fe81694d4056b9934da21a30e1
Andrey Kuprin <plasmaa0@gmail.com>
2025-02-17 13:18:54 +0300

    helix cyryllic keys
    Former-commit-id: 22c4859f8816840211a6793bcd4c3635c76bbc98

--- configuration/homes/common/cli/editor/helix/settings/keys.nix
+++ configuration/homes/common/cli/editor/helix/settings/keys.nix
@@ -1,20 +1,33 @@
-{...}: {
+{lib, ...}: {
   programs.helix.settings.keys = {
-    normal = {
-      C-e = "page_cursor_half_up";
-      C-r = ":config-reload";
-      C-s = ":w";
-      A-j = ["search_selection" "extend_search_next"];
-      C-q = ":bc";
-      C-A-l = ":format";
-      Y = ":clipboard-yank";
-      C-o = ":open ~/.config/helix/config.toml";
-      esc = ["collapse_selection" "keep_primary_selection"];
-      S-up = ["extend_to_line_bounds" "delete_selection" "move_line_up" "paste_before"];
-      S-down = ["extend_to_line_bounds" "delete_selection" "paste_after"];
-      C-left = ["jump_backward"];
-      C-right = ["jump_forward"];
-      ret = "goto_word";
-    };
+    normal = let
+      # map cycyrillic keys to english using helix macro feature
+      # example:
+      # "щ" = "@o" maps cyrillic щ to english o
+      cyrillic = ["й" "ц" "у" "к" "е" "н" "г" "ш" "щ" "з" "х" "ъ" "ф" "ы" "в" "а" "п" "р" "о" "л" "д" "ж" "э" "я" "ч" "с" "м" "и" "т" "ь" "б" "ю"];
+      english = ["q" "w" "e" "r" "t" "y" "u" "i" "o" "p" "[" "]" "a" "s" "d" "f" "g" "h" "j" "k" "l" ";" "'" "z" "x" "c" "v" "b" "n" "m" "," "."];
+      makeBind = cyr: eng: {"${cyr}" = "@${eng}";};
+      cyrillicToEnglishKeysList = lib.zipListsWith makeBind cyrillic english;
+      cyrillicToEnglishKeymap = lib.fold (a: b: a // b) {} cyrillicToEnglishKeysList;
+    in
+      {
+        C-e = "page_cursor_half_up";
+        C-r = ":reload";
+        C-s = ":w";
+        A-j = ["search_selection" "extend_search_next"];
+        C-q = ":bc";
+        C-A-l = ":format";
+        Y = ":clipboard-yank";
+        C-o = ":open ~/.config/helix/config.toml";
+        esc = ["collapse_selection" "keep_primary_selection"];
+        S-up = ["extend_to_line_bounds" "delete_selection" "move_line_up" "paste_before"];
+        S-down = ["extend_to_line_bounds" "delete_selection" "paste_after"];
+        C-left = ["jump_backward"];
+        C-right = ["jump_forward"];
+        ret = "goto_word";
+        "C-=" = "increment";
+        C-minus = "decrement";
+      }
+      // cyrillicToEnglishKeymap;
   };
 }
