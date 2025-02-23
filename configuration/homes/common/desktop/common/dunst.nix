05d1544ccb2986612ec5df63249bcdc648f84ddd
Andrey Kuprin <plasmaa0@gmail.com>
2025-02-23 21:12:44 +0300

    dunst refactor
    Former-commit-id: f0c3fd8b89bfb6b0ed1047c262c02953bc493772

--- configuration/homes/common/desktop/common/dunst.nix
+++ configuration/homes/common/desktop/common/dunst.nix
@@ -5,15 +5,19 @@
   hidpiScalingFactor,
   ...
 }: let
-  c = config.lib.stylix.colors;
-  icons_path = "${builtins.unsafeDiscardStringContext config.gtk.iconTheme.package}/share/icons/${builtins.unsafeDiscardStringContext config.gtk.iconTheme.name}/128x128";
-  symbolic_icons_path = "${builtins.unsafeDiscardStringContext config.gtk.iconTheme.package}/share/icons/${builtins.unsafeDiscardStringContext config.gtk.iconTheme.name}/symbolic";
+  themePkg = config.gtk.iconTheme.package;
+  icons_path = "${builtins.unsafeDiscardStringContext themePkg}/share/icons/${builtins.unsafeDiscardStringContext config.gtk.iconTheme.name}/128x128";
+  symbolic_icons_path = "${builtins.unsafeDiscardStringContext themePkg}/share/icons/${builtins.unsafeDiscardStringContext config.gtk.iconTheme.name}/symbolic";
+  reloadScript = ''
+    ${pkgs.procps}/bin/pkill dunst || true
+    ${pkgs.libnotify}/bin/notify-send -u low "Dunst" "Dunst reloaded" || true
+  '';
 in {
   services.dunst.enable = true;
-  home.activation.dunst_reload = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    run ${pkgs.procps}/bin/pkill dunst || true
-    run ${pkgs.libnotify}/bin/notify-send -u low "Dunst" "Dunst reloaded" || true
-  '';
+  # home.activation.dunst_reload = lib.hm.dag.entryAfter ["writeBoundary"] ''
+  #   run ${pkgs.procps}/bin/pkill dunst || true
+  #   run ${pkgs.libnotify}/bin/notify-send -u low "Dunst" "Dunst reloaded" || true
+  # '';
   # services.dunst.configFile = ../dotfiles/dunst/dunstrc;
   # home.file."${config.xdg.configHome}/dunst/dunstrc".text = ''
   home.file."${config.xdg.configHome}/dunst/logger.sh" = {
@@ -147,180 +151,179 @@ in {
     '';
     executable = true;
   };
-  services.dunst.settings = lib.mkForce (builtins.fromTOML ''
-    [global]
-        monitor = 0
+  xdg.configFile."dunst/dunstrc".onChange = lib.mkForce reloadScript;
+  services.dunst.settings = let
+    c = config.lib.stylix.colors.withHashtag;
+  in {
+    global = {
+      monitor = 0;
 
-        # If this option is set to mouse or keyboard, the monitor option
-        # will be ignored.
-        follow = "mouse"
+      # If this option is set to mouse or keyboard, the monitor option
+      # will be ignored.
+      follow = "mouse";
 
-        # Geometery reference --> [{width}]x{height}[+/-{x}+/-{y}]
-        width = ${toString (builtins.ceil (150 * hidpiScalingFactor))}
-        height = ${toString (builtins.ceil (80 * hidpiScalingFactor))}
-        origin = "top-center"
-        offset = "0x${toString (builtins.ceil (30 * hidpiScalingFactor))}"
+      # Geometery reference --> [{width}]x{height}[+/-{x}+/-{y}]
+      width = builtins.ceil (150 * hidpiScalingFactor);
+      height = builtins.ceil (80 * hidpiScalingFactor);
+      origin = "top-center";
+      offset = "0x${toString (builtins.ceil (30 * hidpiScalingFactor))}";
 
-        # Radius of the four corners of the notification
-        corner_radius = 10
-        progress_bar_corner_radius = 5
+      # Radius of the four corners of the notification
+      corner_radius = 10;
+      progress_bar_corner_radius = 5;
 
-        # Show how many messages are currently hidden (because of geometry).
-        indicate_hidden = "yes"
+      # Show how many messages are currently hidden (because of geometry).
+      indicate_hidden = "yes";
 
-        # Shrink window if it's smaller than the width.  Will be ignored if width is 0.
-        shrink = "yes"
+      # Shrink window if it's smaller than the width.  Will be ignored if width is 0.
+      shrink = "yes";
 
-        # The transparency of the window.  Range: [0; 100].
-        transparency = 80
+      # The transparency of the window.  Range: [0; 100].
+      transparency = 80;
 
-        # The height of the entire notification.  If the height is smaller
-        # than the font height and padding combined, it will be raised
-        # to the font height and padding.
-        # notification_height = 0
+      # The height of the entire notification.  If the height is smaller
+      # than the font height and padding combined, it will be raised
+      # to the font height and padding.
+      # notification_height = 0;
 
-        # Show multiple notifications in the same box
-        separator_height = 2
+      # Show multiple notifications in the same box
+      separator_height = 2;
 
-        # Define a color for the separator.
-        # possible values are:
-        #  * auto: dunst tries to find a color fitting to the background;
-        #  * foreground: use the same color as the foreground;
-        #  * frame: use the same color as the frame;
-        #  * anything else will be interpreted as a X color.
-        separator_color = "auto"
+      # Define a color for the separator.
+      # possible values are:
+      #  * auto: dunst tries to find a color fitting to the background;
+      #  * foreground: use the same color as the foreground;
+      #  * frame: use the same color as the frame;
+      #  * anything else will be interpreted as a X color.
+      separator_color = lib.mkForce "auto";
 
-        # Add vertical padding to the inside of the notification
-        padding = 10
+      # Add vertical padding to the inside of the notification
+      padding = 10;
 
-        # Add horizontal padding for when the text gets long enough
-        horizontal_padding = 10
+      # Add horizontal padding for when the text gets long enough
+      horizontal_padding = 10;
 
-        # The frame color and width of the notification
-        frame_width = 1
+      # The frame color and width of the notification
+      frame_width = 1;
 
-        sort = "yes"
+      sort = "yes";
+      # How long a user needs to be idle for sticky notifications
+      idle_threshold = 120;
 
-        # How long a user needs to be idle for sticky notifications
-        idle_threshold = 120
+      # Font and typography settings
+      font = lib.mkForce config.stylix.fonts.serif.name;
+      alignment = "left";
+      word_wrap = "yes";
 
-        # Font and typography settings
-        font = "${config.stylix.fonts.serif.name}"
-        alignment = "left"
-        word_wrap = "yes"
+      # The spacing between lines.  If the height is smaller than the font height, it will get raised to the font height.
+      line_height = 0;
 
-        # The spacing between lines.  If the height is smaller than the font height, it will get raised to the font height.
-        line_height = 0
+      # Allow some HTML tags like <i> and <u> in notifications
+      markup = "full";
 
-        # Allow some HTML tags like <i> and <u> in notifications
-        markup = "full"
+      # Format for how notifications will be displayed
+      #format = "<b>%s</b>\n%b"
+      format = "<span><b>%s %p</b></span>\\n%b";
 
-        # Format for how notifications will be displayed
-        #format = "<b>%s</b>\n%b"
-        format = "<span><b>%s %p</b></span>\n%b"
+      show_age_threshold = 60;
 
-        show_age_threshold = 60
+      # When word_wrap is set to no, specify where to make an ellipsis in long lines.
+      # Possible values are "start", "middle" and "end".
+      ellipsize = "middle";
 
-        # When word_wrap is set to no, specify where to make an ellipsis in long lines.
-        # Possible values are "start", "middle" and "end".
-        ellipsize = "middle"
+      # Ignore newlines '\n' in notifications.
+      ignore_newline = "no";
 
-        # Ignore newlines '\n' in notifications.
-        ignore_newline = "no"
+      # Stack together notifications with the same content
+      stack_duplicates = "true";
 
-        # Stack together notifications with the same content
-        stack_duplicates = "true"
+      # Hide the count of stacked notifications with the same content
+      hide_duplicate_count = "false";
 
-        # Hide the count of stacked notifications with the same content
-        hide_duplicate_count = "false"
+      # Display indicators for URLs (U) and actions (A).
+      show_indicators = "yes";
 
-        # Display indicators for URLs (U) and actions (A).
-        show_indicators = "yes"
+      # Align icons left/right/off
+      icon_position = "left";
 
-        # Align icons left/right/off
-        icon_position = "left"
+      # Scale larger icons down to this size, set to 0 to disable
+      max_icon_size = 64;
+      icon_corner_radius = 5;
 
-        # Scale larger icons down to this size, set to 0 to disable
-        max_icon_size = 64
-        icon_corner_radius = 5
+      icon_path = "${symbolic_icons_path}/status/:${icons_path}/devices/:${icons_path}/apps/";
 
-        icon_path = "${symbolic_icons_path}/status/:${icons_path}/devices/:${icons_path}/apps/"
+      history_length = 20;
+      sticky_history = "true";
 
-        history_length = 20
-        sticky_history = "true"
+      # Always run rule-defined scripts, even if the notification is suppressed
+      always_run_script = "true";
 
-        # Always run rule-defined scripts, even if the notification is suppressed
-        always_run_script = "true"
+      startup_notification = "true";
 
-        startup_notification = "true"
+      force_xinerama = "false";
 
-        force_xinerama = "false"
-
-        mouse_left_click = "close_current"
-        mouse_right_click = "close_all"
-        mouse_middle_click = "action"
-
-    [urgency_critical]
-        frame_color = "#${c.base08}"
-        background = "#${c.base0F}"
-        foreground = "#${c.base00}"
-        timeout = 0
-
-    [urgency_normal]
-        background = "#${c.base0B}"
-        foreground = "#${c.base01}"
-        frame_color = "#${c.base0B}"
-        timeout = 10
-
-    [urgency_low]
-        # This urgency should be used only
-        # for volume/brightness notification
-        frame_color = "#${c.base05}"
-        foreground = "#${c.base06}"
-        background = "#${c.base01}"
-        timeout = 5
-
-    [z_Cassette]
-        appname="Cassette"
-        background = "#9F3046"
-        foreground = "#DACDD1"
-        frame_color = "#B7B5BA"
-        timeout = 10
-
-    # In process of creating dunstrc all TOML elements are sorted alphabetically
-    # for volume and brightness notifications to work properly
-    # rule for them need to be defined AFTER `urgency_low`
-    # volume and brightness rules are prefixed with z to define them after `urgency_low`
-    [z_volume]
-        appname="volume"
-        background = "#${c.base01}"
-        foreground = "#${c.base0D}"
-        highlight = "#${c.base0D}"
-        alignment="center"
-        max_icon_size = 128
-        # hide_text = true
-        icon_position="off"
-        format = "<span><b>%s %n%%</b></span>\n%b"
-        progress_bar_horizontal_alignment = "center"
-        timeout = 1
-
-    [z_bright]
-        appname="bright"
-        background = "#${c.base01}"
-        foreground = "#${c.base0A}"
-        highlight = "#${c.base0A}"
-        alignment="center"
-        max_icon_size = 128
-        # hide_text = true
-        icon_position="off"
-        format = "<span><b>%s %n%%</b></span>\n%b"
-        progress_bar_horizontal_alignment = "center"
-        timeout = 1
-
-    [logger]
-        summary="*"
-        script="${config.xdg.configHome}/dunst/logger.sh"
-  '');
+      mouse_left_click = "close_current";
+      mouse_right_click = "close_all";
+      mouse_middle_click = "action";
+    };
+    logger = {
+      summary = "*";
+      script = "${config.xdg.configHome}/dunst/logger.sh";
+    };
+    urgency_low = lib.mkForce {
+      # This urgency should be used only
+      # for volume/brightness notification
+      frame_color = c.base05;
+      foreground = c.base06;
+      background = c.base01;
+      timeout = 5;
+    };
+    urgency_normal = lib.mkForce {
+      background = c.base0B;
+      foreground = c.base01;
+      frame_color = c.base0B;
+      timeout = 10;
+    };
+    urgency_critical = lib.mkForce {
+      frame_color = c.base08;
+      background = c.base0F;
+      foreground = c.base00;
+      timeout = 0;
+    };
+    zVolume = {
+      appname = "volume";
+      background = c.base01;
+      foreground = c.base0D;
+      highlight = c.base0D;
+      alignment = "center";
+      max_icon_size = 128;
+      # hide_text = true;
+      icon_position = "off";
+      format = "<span><b>%s %n%%</b></span>\\n%b";
+      progress_bar_horizontal_alignment = "center";
+      timeout = 1;
+    };
+    zBright = {
+      appname = "bright";
+      background = c.base01;
+      foreground = c.base0A;
+      highlight = c.base0A;
+      alignment = "center";
+      max_icon_size = 128;
+      # hide_text = true;
+      icon_position = "off";
+      format = "<span><b>%s %n%%</b></span>\\n%b";
+      progress_bar_horizontal_alignment = "center";
+      timeout = 1;
+    };
+    zMusic = {
+      appname = "Cassette";
+      background = "#9F3046";
+      foreground = "#DACDD1";
+      frame_color = "#B7B5BA";
+      timeout = 10;
+    };
+  };
   home.packages = with pkgs; [
     libnotify
     (writeShellApplication {
