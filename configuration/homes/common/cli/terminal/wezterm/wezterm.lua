b45a476cd622afe4ad12e536571179082314a0a8
Andrey Kuprin <plasmaa0@gmail.com>
2025-02-28 01:26:44 +0300

    wezterm upd
    Former-commit-id: ececbb76acbf9899d67c7c4a62ad2cbd6e471ef8

--- configuration/homes/common/cli/terminal/wezterm/wezterm.lua
+++ configuration/homes/common/cli/terminal/wezterm/wezterm.lua
@@ -57,8 +57,8 @@ return {
   keys = mykeys,
   font = wezterm.font_with_fallback {
     'Iosevka',
-    'JetBrains Mono', 
-    'Terminus', 
+    'JetBrains Mono',
+    'Terminus',
     'Noto Color Emoji',
     harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1' }
   },
@@ -67,13 +67,14 @@ return {
   front_end = "WebGpu", -- when you see colored blocks instead of text
   -- front_end = "OpenGL",
   color_scheme = 'stylix',
+  bold_brightens_ansi_colors = true,
   -- window_background_opacity = 0.75,
   -- color_scheme = 'midnight-in-mojave',
   -- color_scheme = 'Gruvbox dark, soft (base16)',
   default_prog = { 'fish' },
   window_decorations = "RESIZE",
   -- warn_about_missing_glyphs = true,
-  max_fps = 144,
+  max_fps = 240,
   animation_fps = 24,
   initial_cols = 100,
   initial_rows = 30,
