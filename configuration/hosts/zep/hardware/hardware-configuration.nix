b53f606ebc15423cff01170980c64ab717590a9e
Andrey Kuprin <plasmaa0@gmail.com>
2025-03-01 23:50:54 +0300

    zep hardware configuration upd

--- configuration/hosts/zep/hardware/hardware-configuration.nix
+++ configuration/hosts/zep/hardware/hardware-configuration.nix
@@ -14,12 +14,43 @@
   boot.initrd.systemd.enable = true;
   hardware.amdgpu.initrd.enable = true;
   boot.kernelModules = ["kvm-amd"];
+  # https://gitlab.freedesktop.org/drm/amd/-/issues/3545#note_2538277
+  # https://universal-blue.discourse.group/t/linux-bluefin-on-asus-laptop-with-amd-ryzen-ai-vivobook-m5406w/4071
   boot.kernelParams = [
-    "mem_sleep_default=deep"
+    # "iommu=1"
+    # "mem_sleep_default=deep" # Adding mem_sleep_default=deep doesn't work on AMD systems.
+    "amd_pstate=passive"
     "pcie_aspm.policy=powersupersave"
-    "amdgpu.gpu_recovery=1"
-    "amdgpu.dcdebugmask=0x12"
+    "pcie_aspm=off"
     "acpi.prefer_microsoft_dsm_guid=1"
+
+    # Hopefully fixes for where the kernel sometimes hangs when suspending or hibernating
+    #  (Though, I'm very suspicious of the Mediatek Wifi...)
+    "amdgpu.gpu_recovery=1"
+
+    # Can help solve flickering/glitching display issues since Scatter/Gather code was reenabled
+    # One of this two disables PSR
+    # enum DC_DEBUG_MASK {
+    # 	DC_DISABLE_PIPE_SPLIT = 0x1,
+    # 	DC_DISABLE_STUTTER = 0x2,
+    # 	DC_DISABLE_DSC = 0x4,
+    # 	DC_DISABLE_CLOCK_GATING = 0x8,
+    # 	DC_DISABLE_PSR = 0x10,
+    # 	DC_FORCE_SUBVP_MCLK_SWITCH = 0x20,
+    # 	DC_DISABLE_MPO = 0x40,
+    # 	DC_ENABLE_DPIA_TRACE = 0x80,
+    # 	DC_ENABLE_DML2 = 0x100,
+    # 	DC_DISABLE_PSR_SU = 0x200,
+    # 	DC_DISABLE_REPLAY = 0x400,
+    # 	DC_DISABLE_IPS = 0x800,
+    # };
+    # "amdgpu.dcdebugmask=0x12"
+    # "amdgpu.dcdebugmask=0x10"
+    # "amdgpu.dcdebugmask=0x200"
+    "amdgpu.dcdebugmask=0x212"
+
+    # Can help solve flickering/glitching display issues since Scatter/Gather code was reenabled
+    "amdgpu.sg_display=0"
   ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.extraModulePackages = [];
