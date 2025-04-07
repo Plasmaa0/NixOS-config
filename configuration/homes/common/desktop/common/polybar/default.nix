626f7c9305a2b6840fea7f2400e9263f89799b4c
Andrey Kuprin <plasmaa0@gmail.com>
2025-04-07 15:23:58 +0300

    polybar

--- configuration/homes/common/desktop/common/polybar/default.nix
+++ configuration/homes/common/desktop/common/polybar/default.nix
@@ -1,21 +1,37 @@
 {
   config,
   pkgs,
+  lib,
   ...
-}: {
-  programs.polybar.enable = true;
-  # services.polybar.enable = true;
-  services.polybar.package = pkgs.polybar.override {
+}: let
+  polybar = pkgs.polybar.override {
     alsaSupport = true;
     i3Support = true;
     pulseSupport = true;
   };
+in {
   home.file."${config.xdg.configHome}/polybar" = {
     source = ./polybar;
     recursive = true;
+    onChange = "${polybar}/bin/polybar-msg cmd restart || true";
   };
-  services.polybar.script = "polybar &";
+  xsession.windowManager.i3.config.startup = [
+    {
+      notification = false;
+      always = true;
+      command = "${polybar}/bin/polybar-msg cmd restart || true";
+    }
+  ];
+  services.polybar = {
+    enable = true;
+    script = "polybar &";
+    package = polybar;
+  };
   systemd.user.services.polybar = {
-    Install.WantedBy = ["graphical-session.target"];
+    # Install.WantedBy = ["graphical-session.target"];
+    Service = {
+      Restart = lib.mkForce "always";
+      RestartSec = 3;
+    };
   };
 }
