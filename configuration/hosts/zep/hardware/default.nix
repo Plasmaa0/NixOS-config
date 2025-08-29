9d4521617645c8871a18a6f392682f752e4c5cad
Andrey Kuprin <plasmaa0@gmail.com>
2025-08-29 22:57:52 +0300

    playing with CUDA
    Former-commit-id: 7c5d6cf5ec4e2a9bc49986cb77ab2cfcc58f91cf

--- configuration/hosts/zep/hardware/default.nix
+++ configuration/hosts/zep/hardware/default.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: let
   nixos-hardware = builtins.fetchTarball {
@@ -20,6 +21,27 @@ in {
     "${nixos-hardware}/common/pc/laptop"
     "${nixos-hardware}/common/pc/ssd"
   ];
+
+  environment.systemPackages = with pkgs; [
+    cudaPackages.cudatoolkit
+    cudaPackages.cudnn
+    cudaPackages.cuda_cudart
+  ];
+  systemd.services.nvidia-control-devices = {
+    wantedBy = ["multi-user.target"];
+    serviceConfig.ExecStart = "${pkgs.linuxPackages.nvidia_x11.bin}/bin/nvidia-smi";
+  };
+  environment.sessionVariables = {
+    CUDA_HOME = "${pkgs.cudaPackages.cudatoolkit}";
+    LD_LIBRARY_PATH = lib.makeLibraryPath [
+      "${pkgs.cudaPackages.cudatoolkit}"
+      "${pkgs.cudaPackages.cudatoolkit}/lib64"
+      pkgs.cudaPackages.cudnn
+      pkgs.cudaPackages.cuda_cudart
+      pkgs.stdenv.cc.cc.lib
+    ];
+    CUDA_MODULE_LOADING = "LAZY";
+  };
 
   hardware.nvidia = {
     prime = {
