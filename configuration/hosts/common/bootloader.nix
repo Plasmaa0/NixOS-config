b8eabeccab7f6eaa87ac0232cb8002ec7b268b28
Andrey Kuprin <plasmaa0@gmail.com>
2025-10-02 15:37:20 +0300

    secure boot via lanzaboote

--- configuration/hosts/common/bootloader.nix
+++ configuration/hosts/common/bootloader.nix
@@ -1,11 +1,31 @@
-{pkgs, ...}: {
+{
+  inputs,
+  pkgs,
+  lib,
+  ...
+}: {
+  imports = [
+    inputs.lanzaboote.nixosModules.lanzaboote
+  ];
+  environment.systemPackages = [
+    pkgs.sbctl
+  ];
+  boot.loader.systemd-boot.enable = lib.mkForce false;
+  boot.lanzaboote = {
+    enable = true;
+    pkiBundle = "/var/lib/sbctl";
+  };
+  environment.persistence."/persist" = {
+    hideMounts = true;
+    directories = ["/var/lib/sbctl"];
+  };
+
   # Bootloader.
   boot = {
     loader = {
-      systemd-boot.enable = false;
       efi.canTouchEfiVariables = true;
       grub = {
-        enable = true;
+        enable = false;
         efiSupport = true;
         efiInstallAsRemovable = false; # in case canTouchEfiVariables doesn't work for your system
         device = "nodev";
