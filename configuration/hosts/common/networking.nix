b87056c04ab5f0074560d5f98cd6f12224b3a340
Andrey Kuprin <plasmaa0@gmail.com>
2025-10-31 16:42:58 +0300

    small upd
    Former-commit-id: 960e9a917bc7192df38d7362ca8ac5fd48a78dd4

--- configuration/hosts/common/networking.nix
+++ configuration/hosts/common/networking.nix
@@ -1,6 +1,9 @@
 {...}: {
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # Enable networking
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    wifi.powersave = true;
+  };
   programs.amnezia-vpn.enable = true;
 }
