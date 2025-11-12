55215022ed70df006ae2195d0afb2bbd4484cf84
Andrey Kuprin <plasmaa0@gmail.com>
2025-11-12 15:42:04 +0300

    vpn upd
    Former-commit-id: e7a1deeace1160adf96e9fce75f3580a94eeef3d

--- configuration/hosts/common/networking.nix
+++ configuration/hosts/common/networking.nix
@@ -1,9 +1,13 @@
-{...}: {
+{pkgs, ...}: {
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # Enable networking
   networking.networkmanager = {
     enable = true;
     wifi.powersave = true;
   };
-  programs.amnezia-vpn.enable = true;
+  services.v2raya.enable = true;
+  services.v2raya.cliPackage = pkgs.xray;
+  environment.persistence."/persist" = {
+    directories = ["/etc/v2raya"];
+  };
 }
