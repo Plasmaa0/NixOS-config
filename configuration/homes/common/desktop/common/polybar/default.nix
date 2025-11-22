df8954eba77a55fb3164cd99e83a3102d8df571c
Andrey Kuprin <plasmaa0@gmail.com>
2025-11-22 15:21:44 +0300

    fixed polybar colors because stylix changed ~/.Xresources generation

--- configuration/homes/common/desktop/common/polybar/default.nix
+++ configuration/homes/common/desktop/common/polybar/default.nix
@@ -15,6 +15,28 @@ in {
     recursive = true;
     onChange = "${polybar}/bin/polybar-msg cmd restart || true";
   };
+  home.file."${config.xdg.configHome}/polybar/colors.ini".text = let
+    c = config.lib.stylix.colors.withHashtag;
+  in
+    # ini
+    ''
+      [colors]
+      background = ${c.base00}
+      background-alt = ${c.base01}
+      foreground = ${c.base07}
+      foreground-alt = ${c.base03}
+      primary = ${c.base0A}
+      secondary = ${c.base0C}
+      alert = ${c.base09}
+      disabled = ${c.base02}
+      active = ${c.base03}
+      blue = ${c.base0D}
+      purple = ${c.base0E}
+      urgent = ${c.base08}
+
+      underline-1 = ${c.base05}
+      underline-2 = ${c.base0D}
+    '';
   xsession.windowManager.i3.config.startup = [
     {
       notification = false;
