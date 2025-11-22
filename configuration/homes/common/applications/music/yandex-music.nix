922abdfbe25b6e30c95a804fd611d8d7baceb7bb
Andrey Kuprin <plasmaa0@gmail.com>
2025-11-22 15:23:53 +0300

    fixes

--- configuration/homes/common/applications/music/yandex-music.nix
+++ configuration/homes/common/applications/music/yandex-music.nix
@@ -8,7 +8,7 @@
       url = "https://github.com/NixOS/nixpkgs/archive/d66115b18ccf2fbb03da4f2ea8a41499eb8d3136.tar.gz";
       hash = "sha256-XONo9xGd3zGt/iHmIDZ+Uj/FZxRRFmrDrIFqYtzoPnM=";
     }) {
-      inherit (pkgs) system;
+      inherit (pkgs.stdenv.hostPlatform) system;
       config.allowUnfree = true;
     };
 in {
