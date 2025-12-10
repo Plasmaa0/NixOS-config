2bd8cc095a7c601ea8a20622535cf10a0ccbd315
Andrey Kuprin <plasmaa0@gmail.com>
2025-12-10 12:06:05 +0300

    updated obscure-sddm-theme

--- configuration/hosts/common/displayManager.nix
+++ configuration/hosts/common/displayManager.nix
@@ -15,13 +15,13 @@
     stdenvNoCC.mkDerivation
     (_: {
       pname = "obscure-sddm-theme";
-      version = "1.0";
+      version = "1.1";
 
       src = fetchFromGitHub {
         owner = "saatvik333";
         repo = "obscure-sddm-theme";
         rev = "main";
-        hash = "sha256-ZpjdeC8BiriM9ayNOqkOD8eK7NXUU1ONYELuToUEN0E=";
+        hash = "sha256-WMx1SSG1L687ScPSeaZa4FuIoZSdnUidB3n44vyLCwY=";
       };
 
       propagatedUserEnvPkgs = [
@@ -62,22 +62,25 @@
       backgroundFillMode = "aspectCrop"; #aspectCrop, aspectFit, stretch, tile, center
       backgroundOpacity = 1;
       backgroundGlassEnabled = true; # blurring
-      backgroundGlassIntensity = 48; # blurring strength 0-64
+      backgroundGlassIntensity = 75; # blurring strength 0-100
       backgroundTintColor = c.base02;
-      backgroundTintIntensity = 0.25; # 0-1
+      backgroundTintIntensity = 25; # 0-100
 
       # Typography
       fontFamily = config.stylix.fonts.serif.name;
-      baseFontSize = 44;
+      baseFontSize = builtins.ceil (24 * config.hidpi.scalingFactor);
 
       # Controls
       controlCornerRadius = 30; # Corner radius for inputs, selectors, and power buttons
       controlOpacity = 0.75; # Single accent color driving button fills/borders
+      controlBlurEnabled = true;
+      controlBlurIntensity = 75;
       controlAccentColor = c.base03; # Base opacity controlling control fill/border strength
       allowEmptyPassword = false; # Permit logging in without a password
       showUserSelector = false; # Show user selection carousel by default
       showSessionSelector = false; # Show session selection carousel by default
-      randomizePasswordMask = true; # Shuffle IPA mask characters each keystroke
+      randomizePasswordMask = false; # Shuffle IPA mask characters each keystroke
+      autoFocusPassword = true;
       animationDuration = 320; # Base animation length in milliseconds
       passwordFlashLoops = 3; # How many times the password field flashes on error
       passwordFlashOnDuration = 200; # Duration of each flash highlight (ms)
