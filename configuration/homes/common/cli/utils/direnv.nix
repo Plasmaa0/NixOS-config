77467d478e46390e719a9909f434c6000c9a3811
Andrey Kuprin <plasmaa0@gmail.com>
2026-02-02 14:38:11 +0300

    impermanence upd

--- configuration/homes/common/cli/utils/direnv.nix
+++ configuration/homes/common/cli/utils/direnv.nix
@@ -1,9 +1,9 @@
-{config, ...}: {
+{...}: {
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
   };
-  home.persistence."/persist/home/${config.home.username}" = {
+  home.persistence."/persist" = {
     directories = [".local/share/direnv"];
   };
 }
