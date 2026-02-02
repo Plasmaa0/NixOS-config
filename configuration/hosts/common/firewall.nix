25e6c61df2dd0f45ac2d8570b7e56d9681822dd1
Andrey Kuprin <plasmaa0@gmail.com>
2026-02-02 14:42:41 +0300

    firewall fix

--- configuration/hosts/common/firewall.nix
+++ configuration/hosts/common/firewall.nix
@@ -1,8 +1,8 @@
 {lib, ...}: {
   networking.firewall = let
     inherit (lib) range;
-    mindustry = 6567;
-    minecraft = 25565;
+    mindustry = [6567];
+    minecraft = [25565];
     steam-voicechat = range 27014 27020;
     kde-connect = range 1714 1764;
     ports =
