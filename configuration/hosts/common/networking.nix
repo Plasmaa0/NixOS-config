f6b7045aaa4a1397324d7525487a4f685db31cd8
Andrey Kuprin <plasmaa0@gmail.com>
2026-02-03 15:54:24 +0300

    openvpn plugin for network manager

--- configuration/hosts/common/networking.nix
+++ configuration/hosts/common/networking.nix
@@ -1,10 +1,14 @@
-{...}: {
+{pkgs, ...}: {
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # Enable networking
   networking.networkmanager = {
     enable = true;
     wifi.powersave = true;
+    plugins = with pkgs; [
+      networkmanager-openvpn
+    ];
   };
+  programs.openvpn3.enable = true;
   services.v2raya.enable = true;
   environment.persistence."/persist" = {
     directories = ["/etc/v2raya"];
