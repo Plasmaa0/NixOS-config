07c410c223ab8615fd76594a0ca2e1cdb9eb90ce
Andrey Kuprin <plasmaa0@gmail.com>
2026-04-18 12:53:30 +0300

    no nvidia specialization

--- configuration/hosts/zep/hardware/default.nix
+++ configuration/hosts/zep/hardware/default.nix
@@ -61,6 +61,36 @@
   services.xserver.videoDrivers = ["nvidia"];
   hardware.graphics.enable = true;
 
+  specialisation = {
+    powersave = {
+      configuration = {
+        system.nixos.tags = ["powersave"];
+        hardware.nvidia.prime.offload.enable = lib.mkForce false;
+        hardware.nvidia.prime.sync.enable = lib.mkForce false;
+        hardware.nvidia.prime.reverseSync.enable = lib.mkForce false;
+        services.xserver.videoDrivers = lib.mkForce ["amdgpu"];
+        boot.blacklistedKernelModules = lib.mkForce ["nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" "nvidia_uvm"];
+        boot.extraModprobeConfig = lib.mkForce ''
+          blacklist nouveau
+          blacklist nvidia
+          blacklist nvidia_drm
+          blacklist nvidia_modeset
+          blacklist nvidia_uvm
+          options nouveau modeset=0
+        '';
+        services.udev.extraRules = lib.mkForce ''
+          ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+          ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+          ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+          ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+        '';
+        hardware.nvidia.nvidiaSettings = lib.mkForce false;
+        hardware.nvidia.powerManagement.enable = lib.mkForce false;
+        hardware.nvidia.powerManagement.finegrained = lib.mkForce false;
+      };
+    };
+  };
+
   services.udev.extraHwdb = ''
     evdev:name:*:dmi:bvn*:bvr*:bd*:svnASUS*:pn*:*
      KEYBOARD_KEY_ff31007c=f20    # fixes mic mute button
